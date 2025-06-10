48cf96dd7844de53bbaaa3391dd4cb6d938978c4
tri-nguyenducminh <tri.nguyenducminh@ncc.asia>
2025-06-10 14:32:40 +0700

    2025/06/10 add config for @ path

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     allowedHosts: ['b467-14-176-232-186.ngrok-free.app'], // Allow all hosts (e.g., ngrok subdomains)
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 })
