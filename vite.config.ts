1139c7cbd8da77837781d2b89ebd35ef21d8ee08
tri-nguyenducminh <tri.nguyenducminh@ncc.asia>
2025-06-13 10:34:24 +0700

    2025/06/12 add env config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
-import * as path from "path"
+import * as path from 'path'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    allowedHosts: ['cb60-14-176-232-186.ngrok-free.app'],
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+export default defineConfig(({mode}) => {
+  // Load biến môi trường tương ứng với `mode`
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      allowedHosts: [env.VITE_MEZON_SERVER],
     },
-  },
-})
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+  };
+});
